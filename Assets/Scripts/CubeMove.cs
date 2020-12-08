7b7f73c39773c41ed60959de99b19a20f76259ed
Jaani Kekäläinen <44496339+jkekala@users.noreply.github.com>
2020-12-08 10:13:30 +0200

    Some UI-testing
    Did some UI-testing and one image added

---
+++ Assets/Scripts/CubeMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMove : MonoBehaviour
+{
+    //Move variables
+    public float speed;
+    public float bounciness;
+
+    //Action variables
+    public GameObject gun;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
