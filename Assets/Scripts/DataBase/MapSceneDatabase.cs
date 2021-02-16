235fabef9f17f75df86b268f6824515f6aa9df50
moukkari <ilmari.tyrkko@tuni.fi>
2021-02-16 15:24:12 +0200

    Updated marker fetching

--- Assets/Scripts/DataBase/MapSceneDatabase.cs
+++ Assets/Scripts/DataBase/MapSceneDatabase.cs
@@ -58,7 +58,7 @@ public class MapSceneDatabase : MonoBehaviour
     {
 
         FirebaseDatabase.DefaultInstance
-            .GetReference("Ships")
+            .GetReference("Location")
             .ValueChanged += MarkerValueChanged;
     }
 
