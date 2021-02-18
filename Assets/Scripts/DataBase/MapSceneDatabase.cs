247d0ae065c6c2e960e4317ad0de5b788c8337ad
Mwirtanen <miikka.wirtanen@edu.turkuamk.fi>
2021-02-18 18:03:23 +0200

    Updated schedule scripts
    Made major changes to scripts related to schedule functionality. Still work in progress.

--- Assets/Scripts/DataBase/MapSceneDatabase.cs
+++ Assets/Scripts/DataBase/MapSceneDatabase.cs
@@ -1,19 +1,24 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 using Firebase;
 using Firebase.Database;
 using UnityEngine.UI;
-using Firebase.Extensions;
+
+using Newtonsoft.Json;
+
 
 public class MapSceneDatabase : MonoBehaviour
 {
     public Button locationMarker;
     public Button scheduleButton;
     public GameObject mapScreen;
+    public GameObject scheduleScreen;
     List<Button> markerList = new List<Button>();
+    List<Button> scheduleList = new List<Button>();
     DatabaseReference reference;
-    DataSnapshot snapshot;
 
 
     void Awake()
@@ -26,7 +31,7 @@ public class MapSceneDatabase : MonoBehaviour
     void Start()
     {
         LoadMarkers();
-        //LoadScheduleData();
+        LoadScheduleData();
     }
 
     void Update()
@@ -54,7 +59,7 @@ public class MapSceneDatabase : MonoBehaviour
     }
 
     public void LoadMarkers()
-    // Loads markers on the MapScreen.
+    // Loads location data form the database.
     {
 
         FirebaseDatabase.DefaultInstance
@@ -70,7 +75,7 @@ public class MapSceneDatabase : MonoBehaviour
             Debug.LogError(args.DatabaseError.Message);
             return;
         }
-        snapshot = args.Snapshot;
+        DataSnapshot snapshot = args.Snapshot;
 
         if (markerList != null)
         {
@@ -80,7 +85,7 @@ public class MapSceneDatabase : MonoBehaviour
         foreach (DataSnapshot i in snapshot.Children)
         {
             CreateMarker(i.Key, i.Child("Latitude").Value.ToString(), i.Child("Longitude").Value.ToString(),
-				i.Child("type").Value.ToString());
+                i.Child("type").Value.ToString());
 
         }
     }
@@ -94,12 +99,15 @@ public class MapSceneDatabase : MonoBehaviour
         marker.GetComponent<Coordinates>().locationName = name;
         marker.GetComponent<Coordinates>().latitude = latitude;
         marker.GetComponent<Coordinates>().longitude = longitude;
-		
-		if (type.Equals("venue")) {
-			marker.GetComponent<Image>().color = Color.green;
-		} else if (type.Equals("booth")) {
-			marker.GetComponent<Image>().color = Color.yellow;
-		}
+
+        if (type.Equals("venue"))
+        {
+            marker.GetComponent<Image>().color = Color.green;
+        }
+        else if (type.Equals("booth"))
+        {
+            marker.GetComponent<Image>().color = Color.yellow;
+        }
 
         marker.transform.SetParent(mapScreen.transform, false);
 
@@ -109,10 +117,10 @@ public class MapSceneDatabase : MonoBehaviour
     }
 
     public void DestroyMarkers()
-     // Destroys existing markers on the map screen any time the location data changes in the database.
+    // Destroys existing markers on the map screen any time the location data changes in the database.
     {
 
-        for(int i = 0; i < markerList.Count; i++)
+        for (int i = 0; i < markerList.Count; i++)
         {
             Destroy(markerList[i].gameObject);
         }
@@ -122,6 +130,7 @@ public class MapSceneDatabase : MonoBehaviour
     }
 
     public void LoadScheduleData()
+    // Loads schedule data from the database.
     {
         FirebaseDatabase.DefaultInstance
             .GetReference("Schedule")
@@ -137,34 +146,83 @@ public class MapSceneDatabase : MonoBehaviour
             Debug.LogError(args.DatabaseError.Message);
             return;
         }
-        snapshot = args.Snapshot;
+        DataSnapshot snapshot = args.Snapshot;
+
+        if (scheduleList != null)
+        {
+            DestroySchedule();
+        }
+
         foreach (DataSnapshot i in snapshot.Children)
         {
 
             CreateSchedule(
                i.Child("id").Value.ToString(),
+               i.Child("venueId").Value.ToString(),
                i.Child("startTime").Value.ToString(),
                i.Child("endTime").Value.ToString(),
-               i.Child("latitude").Value.ToString(),
-               i.Child("longitude").Value.ToString(),
-               i.Child("address").Value.ToString(),
                i.Child("translations").GetRawJsonValue());
-
         }
 
     }
 
-    public void CreateSchedule(string id, string startTime, string endTime, string latitude, string longitude, string address, string translations)
+    public void CreateSchedule(string id, string venueId, string startTime, string endTime, string translations)
+    // Creates button for schedule menu which shows event name, start- and endtime.
     {
         Button schedule = Instantiate(scheduleButton);
 
         schedule.GetComponent<Schedule>().id = id;
-        schedule.GetComponent<Schedule>().startTime = startTime;
-        schedule.GetComponent<Schedule>().endTime = endTime;
-        schedule.GetComponent<Schedule>().latitude = latitude;
-        schedule.GetComponent<Schedule>().longitude = longitude;
-        schedule.GetComponent<Schedule>().address = address;
-        //schedule.GetComponent<Schedule>().translations = translations
+        schedule.GetComponent<Schedule>().venueId = venueId;
+        schedule.GetComponent<Schedule>().startTime = DateTimeOffset.FromUnixTimeSeconds(Convert.ToInt64(startTime) / 1000).LocalDateTime;
+        schedule.GetComponent<Schedule>().endTime = DateTimeOffset.FromUnixTimeSeconds(Convert.ToInt64(endTime) / 1000).LocalDateTime;
+        schedule.GetComponent<Schedule>().translations = translations;
+
+        var start = schedule.GetComponent<Schedule>().startTime.ToShortTimeString();
+        var end = schedule.GetComponent<Schedule>().endTime.ToShortTimeString();
+
+        var translation = SetScheduleLanguage(translations);
+
+        var child = schedule.transform.GetChild(0);
+        child.GetComponent<Text>().text = translation["event"];
+
+        child = schedule.transform.GetChild(1);
+        child.GetComponent<Text>().text = start + " - " + end;
+
+        scheduleList.Add(schedule);
+
+        //schedule.transform.SetParent(scheduleScreen.transform, false);
 
     }
+
+    public Dictionary<string, string> SetScheduleLanguage(string translations)
+    // Sets schedule language based on user settings.
+    {
+        Dictionary<string, Dictionary<string, string>> languages =
+            JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, string>>>(Convert.ToString(translations));
+
+        string language = "fi";  // Needs to get the language from settings
+
+        foreach (var item in languages)
+        {
+            if (language == item.Key)
+            {
+                var translation = item.Value;
+                return translation;
+            }
+        }
+        return null;
+    }
+
+    public void DestroySchedule()
+    // Destroys existing schedule button on the schedule screen any time the schedule data changes in the database.
+    {
+
+        for (int i = 0; i < scheduleList.Count; i++)
+        {
+            Destroy(scheduleList[i].gameObject);
+        }
+
+        scheduleList.Clear();
+    }
+
 }
--- Assets/Scripts/DataBase/Schedule.cs
+++ Assets/Scripts/DataBase/Schedule.cs
@@ -1,18 +1,16 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Schedule : MonoBehaviour
 {
     public string id;
-    public string startTime;
-    public string endTime;
-    public string latitude;
-    public string longitude;
-    public string address;
+    public string venueId;
+    public DateTime startTime;
+    public DateTime endTime;
+    public string translations;
 
-    public Dictionary<string, object> translations;
-    
     public Schedule()
     {
 
