1afb328096159c5e9093e3313c7dfbbc0f78a70a
Mwirtanen <miikka.wirtanen@edu.turkuamk.fi>
2021-02-25 15:15:54 +0200

    Fixed the issue with Markers
    Fixed the issue with markers throwing error when clicked.

--- Assets/Scripts/DataBase/MarkerButton.cs
+++ Assets/Scripts/DataBase/MarkerButton.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 using UnityEngine.UI;
 
 
@@ -10,53 +11,30 @@ public class MarkerButton : MonoBehaviour
     public Button button;
     public GameObject confScreen;
     
-    private void Awake()
-    {
-        FindConfScreen();
-    }
     private void Start()
     {
+        confScreen = GameObject.Find("ConfirmationScreen");
         button.onClick.AddListener(MarkerClick);            
     }
+
     public void MarkerClick()
         //Sets confirmationscreen visible and writes text to confirmation textbox. Also adds the location data to Coordinatedata class
 
     {      
         string locationName = gameObject.GetComponent<Coordinates>().locationName;
-        var child = confScreen.transform.GetChild(3);
-        child.GetComponent<Text>().text = "Navigate to " + locationName + "?";
+        var child = confScreen.transform.GetChild(0).transform.GetChild(0);
+        child.GetComponent<TMPro.TextMeshProUGUI>().text = "Navigate to " + locationName + "?";
+        RectTransform Pos = confScreen.GetComponent<RectTransform>();
+        Pos.SetSiblingIndex(4);
         confScreen.SetActive(true);
-
-
+      
         CoordinateData.locationName = gameObject.GetComponent<Coordinates>().locationName;
         CoordinateData.latitude = gameObject.GetComponent<Coordinates>().latitude;
         CoordinateData.longitude = gameObject.GetComponent<Coordinates>().longitude;
 
     }
 
-    public void FindConfScreen() {
-        // Finds the ConfirmationScreen gameobject and places it in the confScreen variable
-
-                                                                                    //Matti:
-                                                                                    //SUGGESTION: make a public static instance SINGLETON of the mapscenemanager
-                                                                                    // and store all references in that, all other scripts can find the references from that singleton with 
-                                                                                    //t.ex: confScreen=MapSceneManager.instance.confirmationScreen;
-
-                                                                                    //Can be made cheaper with:
-                                                                                    //confScreen = GameObject.Find("ConfirmationScreen");
-
-        GameObject[] objects = Resources.FindObjectsOfTypeAll(typeof(GameObject)) as GameObject[];
-
-        foreach (var i in objects)
-        {
-            if (i.name == "ConfirmationScreen")
-            {
-                confScreen = i.gameObject;
-                break;
-            }
-        }
-    }
-
+    
     /*public void showInformation()
     {
         if (destroy == true)
