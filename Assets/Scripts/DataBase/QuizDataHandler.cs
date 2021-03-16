36f882a0dd231a684a36f2f0d16d7c3a3cfbc4c4
Mwirtanen <miikka.wirtanen@edu.turkuamk.fi>
2021-03-16 17:01:25 +0200

    Updated Quiz data fetching
    Updated quiz data fetching so that that the information it fetches from database is always up to date.

--- Assets/Scripts/DataBase/QuizDataHandler.cs
+++ Assets/Scripts/DataBase/QuizDataHandler.cs
@@ -19,42 +19,19 @@ public class QuizDataHandler : MonoBehaviour
     }
 
     public static void LoadQuizz(string id, string language)
-        //Loads target quiz from the database.
-    {
-        FirebaseDatabase.DefaultInstance
-       .GetReference("Quiz").Child(id).Child(language)
-       .GetValueAsync().ContinueWith(task =>
-       {
-           if (task.IsFaulted)
-           {
-               Debug.Log("Something went wrong when fetching quizz");
-           }
-           else if (task.IsCompleted)
-           {
-               DataSnapshot snapshot = task.Result;
-
-               Quiz.id = id;
-               Quiz.quizText = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, string>>>(snapshot.GetRawJsonValue());
-
-           }
-       });
-
-    }
-/*
-    public static void LoadQuizz(string id, string language)
-    // Loads location data form the database.
+    // Loads quiz data from the database.
     {
 
         FirebaseDatabase.DefaultInstance
-            .GetReference("Quizz").Child(id).Child(language)
+            .GetReference("Quiz").Child(id).Child(language)
             .ValueChanged += QuizzValueChanged;
 
-       FirebaseDatabase.DefaultInstance
-           .GetReference("Quizz").Child(id).Child(language)
+        FirebaseDatabase.DefaultInstance
+           .GetReference("Quiz").Child(id).Child(language)
            .ValueChanged -= QuizzValueChanged;
     }
 
-    public static void QuizzValueChanged(object sender, ValueChangedEventArgs args)
+    static void QuizzValueChanged(object sender, ValueChangedEventArgs args)
     //Listens to changes in the database and loads a new snapshot when data changes.
     {
         if (args.DatabaseError != null)
@@ -65,16 +42,6 @@ public class QuizDataHandler : MonoBehaviour
         DataSnapshot snapshot = args.Snapshot;
 
         Quiz.id = snapshot.Key;
-
-        string json = snapshot.GetRawJsonValue();
-
-        //var dict = JsonConvert.DeserializeObject<Dictionary<string, object>>(json);
-
-        foreach (var question in snapshot.Children)
-        {
-
-
-        }
+        Quiz.quizText = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, string>>>(snapshot.GetRawJsonValue());
     } 
-*/
 }
