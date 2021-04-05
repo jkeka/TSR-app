d757085cf6256258817110e30f03c6a2f7730f78
Mwirtanen <miikka.wirtanen@edu.turkuamk.fi>
2021-04-05 12:47:39 +0300

    Added script for glossary
    Added initial script for glossary. Will be modified once the UI is in place.

---
+++ Assets/Scripts/Glossary.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Newtonsoft.Json;
+
+public class Glossary : MonoBehaviour
+{
+    private string[] paths = {"Assets/Data/joutsen.json", "Assets/Data/sailing.json" };
+    public Button suuretPurjeLaivat;
+    public Button purjehdusTunnissa;
+    // Start is called before the first frame update
+    void Start()
+    {       
+        suuretPurjeLaivat.onClick.AddListener(delegate { LoadGlossary(paths[0]); });
+        purjehdusTunnissa.onClick.AddListener(delegate { LoadGlossary(paths[1]); });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void LoadGlossary(string path)
+    // Loads the selected glossary data on screen
+    {
+
+        string s = "";
+
+        StreamReader reader = new StreamReader(path);
+        string json = reader.ReadToEnd();
+        Dictionary<string, string> glossary = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
+        reader.Close();
+
+        foreach (KeyValuePair<string, string> entry in glossary)
+        {
+            s = s + entry.Key + ": " + entry.Value + "\n";
+        }
+
+        Debug.Log(s);
+    }   
+}
